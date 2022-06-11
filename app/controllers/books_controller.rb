836840aed7e5aa1cb4825221f05884602d8f1b90
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-06-11 15:14:38 +0000

    [Trying]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book =Book.all
+    @books =Book.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get 'books/new'
-  get 'books/create'
-  get 'books/show'
-  get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   resources :books
 end
