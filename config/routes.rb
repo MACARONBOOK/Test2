ccfb7ae756fc72205c91abc4f55b3e14e63775d7
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-06-11 03:26:33 +0000

    [Try]test2

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/' => 'homes#top'
+  resources :books
+end
